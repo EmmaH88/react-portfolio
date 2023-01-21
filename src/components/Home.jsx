5d9c8acba2eaf85e659b9f7da9d8efee51556438
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-21 12:33:59 +0000

    added social icons and styled, added imgs,

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HiArrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return <div className="w-full h-screen bg-[#0a192f]"></div>;
+};
+
+export default Home;
