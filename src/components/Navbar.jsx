b4995f437c98175be8ce96d41b519afbcc9d353e
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-22 10:22:56 +0000

    added links to onClick state for mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,16 +16,13 @@ const Navbar = () => {
         <img src={Logo} alt="Logo" style={{ width: "80px" }} />
       </div>
 
-      {/*menu*/}
       <ul className="hidden md:flex">
         <li>
-          {" "}
           <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          {" "}
           <Link to="about" smooth={true} duration={500}>
             About
           </Link>
@@ -47,12 +44,10 @@ const Navbar = () => {
         </li>
       </ul>
 
-      {/* hamburger*/}
       <div onClick={handleClick} className="md:hidden z-10">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
-      {/* mobile menu*/}
       <ul
         className={
           !nav
@@ -60,14 +55,36 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Work</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
-      {/*social icons */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]">
