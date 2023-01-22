ca3df1105c2d07b96fc05dd58b2577e3e54354e3
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-22 11:08:01 +0000

    changed colours and font-size in work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,13 +5,13 @@ import reactweather from "../assests/reactweather.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen bg-[#113f67] text-[#ccd6f6]">
+    <div name="work" className="w-full md:h-screen bg-[#141010] text-[#ccd6f6]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-grey-300 border-pink-600">
+          <p className="text-4xl font-bold inline border-b-4 text-[#C4195D] border-pink-600">
             Work
           </p>
-          <p className="py-6">Check out some of my recent work</p>
+          <p className="text-xl py-6">Check out some of my recent work</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
