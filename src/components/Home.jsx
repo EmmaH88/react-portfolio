ffd7e0ab2e7a3d302a41b3d0e65e3e95091197ff
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-22 11:23:12 +0000

    removed button from home,

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
@@ -12,7 +11,7 @@ const Home = () => {
         <h2 className="text-4xl sm:text-7xl font-bold text-[#680647]">
           I'm a junior front end developer
         </h2>
-        <p className="text-[#F70776] text-xl  py-4 max-w-[700px]">
+        <p className="text-[#ccd6f6] text-xl  py-4 max-w-[700px]">
           There are many reasons why I love front-end development. <br /> I like
           to create visually appealing and user-friendly websites and web
           applications, and the ability to directly see and interact with the
@@ -21,14 +20,6 @@ const Home = () => {
           problem-solving aspect of front-end development both challenging and
           ultimately rewarding.
         </p>
-        <div>
-          <button className="text-[#F70776] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#141010] hover:border-pink-600">
-            View My Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
-        </div>
       </div>
     </div>
   );
