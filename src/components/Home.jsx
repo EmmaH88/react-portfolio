0f982dc12894720b76105d2df3a1a5d587b89a24
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-22 10:55:45 +0000

    changed colours in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,16 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div name="home" className=" w-full h-screen bg-[#113f67]">
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-3xl text-[#E7EAF6]">Hi, my name is </p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+    <div name="home" className=" w-full h-screen bg-[#141010]">
+      <div className="max-w-[1000px] mx-auto px-10 flex flex-col justify-center h-full">
+        <p className="text-3xl text-[#F70776] pt-12 pb-4">Hi, my name is </p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#C4195D]">
           Emma Hanby
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#680647]">
           I'm a junior front end developer
         </h2>
-        <p className="text-[#E7EAF6] text-xl  py-4 max-w-[700px]">
+        <p className="text-[#F70776] text-xl  py-4 max-w-[700px]">
           There are many reasons why I love front-end development. <br /> I like
           to create visually appealing and user-friendly websites and web
           applications, and the ability to directly see and interact with the
@@ -22,7 +22,7 @@ const Home = () => {
           ultimately rewarding.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+          <button className="text-[#F70776] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#141010] hover:border-pink-600">
             View My Work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3 " />
